893ae5c04b543fe63290097706c57461e6c345f5
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-04 18:36:08 +0530

    REFACTORED: main method to call inventory manager

--- src/com/bridgelabz/inventorymanagement/InventoryManagementMain.java
+++ src/com/bridgelabz/inventorymanagement/InventoryManagementMain.java
@@ -6,26 +6,20 @@ import java.util.Scanner;
 public class InventoryManagementMain {
 
 	public static void main(String[] args) {
-		MyLinkedList<Inventory> inventoryList = new MyLinkedList<Inventory>();
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter the number of Inventory that you want to add");
-		int count  = sc.nextInt();
+		System.out.println("welcome to inventory management");
 		
-		for(int i=0;i<count;i++) {
-			System.out.println("Enter name of inventory");
-			String name  = sc.next();
-			System.out.println("Enter weight in kg");
-			double weight  = sc.nextDouble();
-			System.out.println("Enter the price per kg");
-			double price = sc.nextDouble();
-			Inventory inventory = new Inventory(name, weight, price);
-			InventoryNode newNode = new InventoryNode(inventory);
-			inventoryList.add(newNode);
-		}
+		InventoryManager inventoryManager = new InventoryManager();
 		
-		inventoryList.display();
+		inventoryManager.readInventory();
+		System.out.println("entred inventory is");
+		inventoryManager.display();
+		
+		inventoryManager.ComputeValue();
+		inventoryManager.display();
+		
 
 	}
 
